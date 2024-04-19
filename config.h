1dec2b99aa860d1a5bad70b8916c8eda30569204
RandomGuy1520 <131367306+RandomGuy1520@users.noreply.github.com>
2024-04-19 17:25:47 +0800

    Update config.h

--- config.h
+++ config.h
@@ -6,14 +6,14 @@
 #define assert(x, y) if (!(x)) { throw(y); }
 
 const enum status { BLANK, X, O, BANNED };
-const enum mode { CVSC, CVSP, PVSC, PVSP };
+const enum mode { CVSC, CVSP, PVSC, PVSP, GET_FROM_USER_INPUT };
 const enum lang { ENG, CHI };
 
 const int MAXN = 9, MAXK = 3, MAXDEPTH = 10; // MAXDEPTH is changeable to control the depth of computer calculation of the board.
 const bool PRINT_EVAL = true, USING_HEURISTICS = true;
 const double uninitialized = 1e9;
-const mode playing_mode = CVSC; // Playing mode is changeable
 const lang version = CHI;
+mode playing_mode = GET_FROM_USER_INPUT; // Playing mode is changeable
 
 #ifdef USING_NORMAL_BOARD
 const double heur1 = 1.0 / 6.5, heur2 = 1.0 / 2;
