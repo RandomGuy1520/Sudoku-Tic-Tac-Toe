1b670b6fcd786f09e02c23af1028ff981c5799d3
RandomGuy1520 <131367306+RandomGuy1520@users.noreply.github.com>
2024-04-26 21:12:32 +0800

    Change heuristics for 3*3 board

--- config.h
+++ config.h
@@ -17,5 +17,5 @@ const int depths[10] = { 0, 0, 16, 11, 7, 6, 4, 3 };
 const int MAXDEPTH = depths[MAXK];
 
 #ifdef USING_NORMAL_BOARD
-const double heur1 = 1.0 / 6.5, heur2 = 1.0 / 2; // Initial heuristics for 9 * 9 board (best by test).
+const double heur1 = 1.0 / 6, heur2 = 1.0 / 2; // Initial heuristics for 9 * 9 board (best by test).
 #endif
